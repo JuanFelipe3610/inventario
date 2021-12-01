1473ddfcd15092c3017c61a6095ed0ee2f22c01a
JuanFelipe3610 <jpena3610@gmail.com>
2021-12-01 01:05:31 -0500

    Revert "Implementacion de react"
    This reverts commit a56fcd44fedfc65de309824ac0b1c44712fe9bd7.

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -1,13 +1,12 @@
             </div>
         </section>
     </div>
-    <script src="/resources/js/common/jquery.js"></script>
-    <script src="/resources/js/common/bootstrap.min.js"></script>
+    <script src="/resources/js/jquery.js"></script>
+    <script src="/resources/js/bootstrap.min.js"></script>
     <script src="/resources/lib/paginationjs/src/pagination.js"></script>
-    <script type="module"  src="/resources/js/common/scripts.js"></script>
-    <script src="/resources/js/common/bootstrap-table.min.js"></script>
-    <script src="/resources/js/common/bootstrap-table-group-by.js"></script>
-    <script src="dist/main.js" charset="utf-8"></script>
+    <script src="/resources/js/scripts.js"></script>
+    <script src="/resources/js/bootstrap-table.min.js"></script>
+    <script src="/resources/js/bootstrap-table-group-by.js"></script>
     <?=$script?>
     <em>&copy; 2021</em>
 </body>
