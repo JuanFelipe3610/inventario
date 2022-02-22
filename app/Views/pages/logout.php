25cd022f50f4d84e6211836751b1b4e30d996669
JuanFelipe3610 <jpena3610@gmail.com>
2022-02-21 23:12:42 -0500

    se elimino todo

--- app/Views/pages/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-    require_once('../config.php');
-    use controller\session;
-    session::destroy();
-    header('Location: login');
-?>
--- app/Views/templates/loginFooter.php
+++
@@ -1,9 +0,0 @@
-    <script src="/resources/js/jquery.js"></script>
-    <script src="/resources/js/bootstrap.min.js"></script>
-    <script src="/resources/lib/paginationjs/src/pagination.js"></script>
-    <script src="/resources/js/scripts.js"></script>
-    <script src="/resources/js/bootstrap-table.min.js"></script>
-    <script src="/resources/js/bootstrap-table-group-by.js"></script>
-    <?=$script?>
-</body>
-</html>
